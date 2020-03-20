a24fcf2aab5f240cec7a39a4bd911d12ae19c697
Lenovo <Lenovo@LAPTOP-FP4RF41K>
2020-03-20 15:16:51 +0800

    第一次提交

---
+++ Unit2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("第一个代码");
+        }
+    }
+}
