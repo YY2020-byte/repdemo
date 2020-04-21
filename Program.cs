c9cf8ba547a142d3c8255d69223af803cdd979ab
Lenovo <Lenovo@LAPTOP-FP4RF41K>
2020-04-21 18:09:49 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper.Contrib.Extensions;
+
+namespace DapperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["dapperConnStr"].ConnectionString))
+            {
+                //Student student = new Student()
+                //{
+                //    Name = "张三",
+                //    Age = 20,
+                //    Remark = "测试Dapper"
+                //};
+
+                //db.Execute("insert into Student(name, age, remark) values(@name,@age,@remark)", new {name="XC", age=8,remark="fsdgfsdg" });
+
+                db.Insert(new Student() { Id=9, Name = "1708A", Age = 3, Remark = "very good" });
+
+
+
+                //db.Delete<Student>(new Student() { Id = 1 });
+
+                //db.Update<Student>(new Student() { Id = 9, Name = "1708A", Age = 2, Remark = "very very very good" });
+               
+                Student student = db.Get<Student>(3);
+
+                //var students = db.Query<Student>("select * from student");
+
+                // //Student student = db.QueryFirstOrDefault<Student>("select * from student");
+
+
+                // string query = "select * from student WHERE Id = @IdH AND NAME = @NAME";
+                // var student3 = db.QuerySingleOrDefault<Student>(query, new { IdH = 2, NAME = "张三" });
+
+
+                //Student student2 = db.QuerySingleOrDefault<Student>("select * from student where Id = 2");
+
+
+
+
+            }
+        }
+    }
+}
+
+
+
---
+++ Student.cs
@@ -0,0 +1,20 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDemo
+{
+
+    [Table("Student")]
+    public class Student
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Remark { get; set; }
+    }
+}
